a260ef5720ac29c0826cd442a90dea802d33f0ce
nervous solider <hammadsharifsharif@gmail.com>
2026-07-15 16:24:25 +0500

    made some simple changes

--- m_3_3_4.v
+++ m_3_3_4.v
@@ -1,1 +1,13 @@
+//Multipliers: 
+//1. Design a module that will multiply two 3-bit signed inputs and the output will be a 4-bit signed
+
+module multiplier( num_1 , num_2 , ans , clk, reset );
+input [2:0] num_1 ;
+input [2:0] num_2 ;
+output [3:0] ans ; 
+input clk, reset ;
+
+assign ans = num_1 * num_2 ;
+
+endmodule 
 
--- m_3_3_4_tb.v
+++ m_3_3_4_tb.v
@@ -1,1 +1,19 @@
+module tb_muli_kh ;
+reg [2:0] num_1 ;
+reg [2:0] num_2 ;
+wire [3:0] ans ;
+reg clk ;
+reg reset ;
+multiplier tb_m (num_1 , num_2,  ans , clk , reset) ;
 
+initial begin 
+num_1 = 3'sd3 ;
+num_2 = 3'sd0 ;
+#20 ;
+num_1 = 3'sd3 ;
+num_2 = 3'sd2 ;
+#10 ;
+num_1 = 3'd2 ;
+num_2 = 3'd1 ;
+end
+endmodule
