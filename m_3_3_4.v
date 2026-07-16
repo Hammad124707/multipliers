4628533a89ce72921642236621367954a2753994
Your Name <hammadsharifsharif@gmail.com>
2026-07-16 09:06:45 +0500

    added signed while initializing

--- m_3_3_4.v
+++ m_3_3_4.v
@@ -2,13 +2,12 @@
 //1. Design a module that will multiply two 3-bit signed inputs and the output will be a 4-bit signed
 
 module multiplier( num_1 , num_2 , ans , clk, reset );
-input [2:0] num_1 ;
-input [2:0] num_2 ;
-output [3:0] ans ; 
+input signed [2:0] num_1 ;
+input signed [2:0] num_2 ;
+output signed [3:0] ans ; 
 input clk, reset ;
 
 assign ans = num_1 * num_2 ;
 //checking the github cha
 
 endmodule 
-
