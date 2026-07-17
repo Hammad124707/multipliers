236859669a33b73dcf0b144c709fc50f34f7229f
Hammad <82674789+Hammad124707@users.noreply.github.com>
2026-07-17 08:52:35 +0500

    Refactor mul2 module to include clock and reset

--- m_2_4_4.v
+++ m_2_4_4.v
@@ -1,12 +1,15 @@
-//
-// Design a module that will multiply one 4-bit signed input and one 2 bit signed input and the output will be 4 bit signed. 
-
-module mul2 (in1, in2 ,out) ; 
-input signed[3:0] in1 ;
-input signed [1:0] in2 ;
-output signed [3:0] out ;
-assign out = in1 * in2 ;
-endmodule 
-
+module mul2 (in1, in2, out, clk, reset);
+    input signed [3:0] in1;
+    input signed [1:0] in2;
+    output reg signed [3:0] out;
+    input clk;
+    input reset;
  
-
+    always @(posedge clk) begin
+        if (reset)
+            out <= 0;
+        else
+            out <= in1 * in2;
+    end
+endmodule
+ 
