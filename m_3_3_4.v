a031f41228f5ed1ff18272303d02b3398d337513
Your Name <hammadsharifsharif@gmail.com>
2026-07-17 08:41:17 +0500

    Added cloclk

--- m_3_3_4.v
+++ m_3_3_4.v
@@ -1,13 +1,15 @@
-//Multipliers: 
-//1. Design a module that will multiply two 3-bit signed inputs and the output will be a 4-bit signed
-
 module multiplier( num_1 , num_2 , ans , clk, reset );
-input signed [2:0] num_1 ;
-input signed [2:0] num_2 ;
-output signed [3:0] ans ; 
-input clk, reset ;
-
-assign ans = num_1 * num_2 ;
-//checking the github cha
-
-endmodule 
+    input signed [2:0] num_1 ;
+    input signed [2:0] num_2 ;
+    output reg signed [3:0] ans ;
+    input clk, reset ;
+ 
+    always @(posedge clk) begin
+        if (reset)
+            ans <= 0;
+        else
+            ans <= num_1 * num_2;
+    end
+ 
+endmodule
+ 
--- m_3_3_4_tb.v
+++ m_3_3_4_tb.v
@@ -1,19 +1,39 @@
-module tb_muli_kh ;
-reg [2:0] num_1 ;
-reg [2:0] num_2 ;
-wire [3:0] ans ;
-reg clk ;
-reg reset ;
-multiplier tb_m (num_1 , num_2,  ans , clk , reset) ;
+`timescale 1ns/1ps
+module tb_muli_kh;
+    reg signed [2:0] num_1;
+    reg signed [2:0] num_2;
+    wire signed [3:0] ans;
+    reg clk;
+    reg reset;
 
-initial begin 
-num_1 = 3'sd3 ;
-num_2 = 3'sd0 ;
-#20 ;
-num_1 = 3'sd3 ;
-num_2 = 3'sd2 ;
-#10 ;
-num_1 = 3'd2 ;
-num_2 = 3'd1 ;
-end
+    multiplier tb_m (num_1, num_2, ans, clk, reset);
+
+    initial clk = 0;
+    always #5 clk = ~clk;
+
+    initial begin
+        reset = 1;
+        num_1 = 0;
+        num_2 = 0;
+        @(posedge clk);
+        @(posedge clk);
+        reset = 0;
+
+        @(posedge clk);
+        num_1 = 3'sd3;
+        num_2 = 3'sd0;
+        @(posedge clk);
+        num_1 = 3'sd3;
+        num_2 = 3'sd2;
+        @(posedge clk);
+        num_1 = 3'sd2;
+        num_2 = 3'sd1;
+        @(posedge clk);
+        @(posedge clk);
+
+        $finish;
+    end
+
+    always @(posedge clk)
+        $display("t=%0t reset=%b num_1=%d num_2=%d ans=%d", $time, reset, num_1, num_2, ans);
 endmodule
